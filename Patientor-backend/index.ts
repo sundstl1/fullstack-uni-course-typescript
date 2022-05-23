ba75fdc3eb8bedc261bf17009a96e5afe0be323b
Linus Sundström <linus.sundstrom@aalto.fi>
2022-05-23 20:00:53 +0300

    adding FE files and fixing cors

--- Patientor-backend/index.ts
+++ Patientor-backend/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
+
+const allowedOrigins = ["http://localhost:3000", "https://localhost:3000"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
