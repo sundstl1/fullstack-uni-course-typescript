beb267a4094f3b1991e57b990ee049c623b23f77
Linus Sundström <linus.sundstrom@aalto.fi>
2022-05-25 13:19:21 +0300

    9.15

--- courses-frontend/my-app/src/App.tsx
+++ courses-frontend/my-app/src/App.tsx
@@ -2,45 +2,139 @@ import { stringify } from "querystring";
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CourseNormalPart extends CourseDescriptionPart {
+    type: "normal";
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CourseDescriptionPart {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+
+  interface CourseDescriptionPart extends CoursePartBase {
+    description: string;
+  }
+
+  interface CourseRequirementsPart extends CourseDescriptionPart {
+    requirements: string[];
+    type: "special";
+  }
+
+  type CoursePart =
+    | CourseNormalPart
+    | CourseProjectPart
+    | CourseSubmissionPart
+    | CourseRequirementsPart;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal",
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special",
     },
   ];
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  const Part = ({ part }: { part: CoursePart }) => {
+    const heading = (
+      <b>
+        {part.name} {part.exerciseCount}
+      </b>
+    );
+    switch (part.type) {
+      case "normal":
+        return (
+          <p>
+            {heading}
+            <br />
+            <i>{part.description}</i>
+          </p>
+        );
+      case "groupProject":
+        return (
+          <p>
+            {heading}
+            <br />
+            project exercises {part.exerciseCount}
+          </p>
+        );
+      case "submission":
+        return (
+          <p>
+            {heading}
+            <br />
+            <i>{part.description}</i>
+            <br />
+            submit to {part.exerciseSubmissionLink}
+          </p>
+        );
+      case "special":
+        return (
+          <p>
+            {heading}
+            <br />
+            <i>{part.description}</i>
+            <br />
+            required skils: {part.requirements.join(", ")}
+          </p>
+        );
+      default:
+        return assertNever(part);
+    }
+  };
 
   const Header = ({ name }: { name: string }) => {
     return <h1>{name}</h1>;
   };
 
-  interface CoursePart {
-    name: string;
-    exerciseCount: number;
-  }
-
-  const courseData: Array<CoursePart> = courseParts.map((obj) => {
-    const object: CoursePart = {
-      name: obj.name,
-      exerciseCount: obj.exerciseCount,
-    };
-    return object;
-  });
-
   const Content = ({ parts }: { parts: CoursePart[] }) => {
     return (
       <>
         {parts.map((part) => (
-          <p key={part.name}>
-            {part.name} {part.exerciseCount}
-          </p>
+          <Part key={part.name} part={part} />
         ))}
       </>
     );
@@ -58,7 +152,7 @@ const App = () => {
   return (
     <div>
       <Header name={courseName} />
-      <Content parts={courseData} />
+      <Content parts={courseParts} />
       <Total parts={courseParts} />
     </div>
   );
